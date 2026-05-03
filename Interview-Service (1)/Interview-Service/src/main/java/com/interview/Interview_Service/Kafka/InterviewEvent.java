4957f5998db23112f05aa1ac6a4f1c870abe588f
SWASTIK <swastikthakur2604@gmail.com>
2026-05-03 16:06:30 +0530

    updated lombok

--- Interview-Service (1)/Interview-Service/src/main/java/com/interview/Interview_Service/Kafka/InterviewEvent.java
+++ Interview-Service (1)/Interview-Service/src/main/java/com/interview/Interview_Service/Kafka/InterviewEvent.java
@@ -11,19 +11,5 @@ public class InterviewEvent implements Serializable {
     private Long interviewId;
     private String message;
 
-    public Long getInterviewId() {
-        return interviewId;
-    }
-
-    public void setInterviewId(Long interviewId) {
-        this.interviewId = interviewId;
-    }
-
-    public String getMessage() {   // ✅ THIS IS MISSING IN YOUR CASE
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
+    
 }
